981245a25001c9b847a6d4f36627e1a02c3bd6e0
Gaspare <gaspare@coolshop.it>
2021-10-26 17:19:12 +0200

    Init project

---
+++ App21/App21/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace App21
+{
+	public partial class App : Application
+	{
+		public App() {
+			InitializeComponent();
+
+			SQLitePCL.Batteries_V2.Init();
+
+			var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library");
+			
+			var sqLiteDbConnection = new SqliteConnection($"FileName={Path.Combine(path, "test.db")}");
+                        
+			sqLiteDbConnection.Open();
+            
+			var commandPragma = sqLiteDbConnection.CreateCommand();
+			commandPragma.CommandText = "PRAGMA key = 'testKey';";
+			commandPragma.ExecuteNonQuery();
+
+			var commandCreateTable = sqLiteDbConnection.CreateCommand();
+            commandCreateTable.CommandText = "CREATE TABLE testTable(id integer)";
+            commandCreateTable.ExecuteNonQuery();
+
+			var commandQuery = sqLiteDbConnection.CreateCommand();
+			commandQuery.CommandText = "SELECT * FROM testTable;";
+			var queryResult = commandQuery.ExecuteScalar();
+            
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart() {
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep() {
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume() {
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ App31/App31/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace App31
+{
+	public partial class App : Application
+	{
+		public App() {
+			InitializeComponent();
+
+			var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library");
+			
+			var sqLiteDbConnection = new SqliteConnection($"FileName={Path.Combine(path, "test.db")}");
+			
+			sqLiteDbConnection.Open();
+            
+			var commandPragma = sqLiteDbConnection.CreateCommand();
+			commandPragma.CommandText = "PRAGMA key = 'testKey';";
+			commandPragma.ExecuteNonQuery();
+			
+			var commandMigrate = sqLiteDbConnection.CreateCommand();
+			commandMigrate.CommandText = "PRAGMA cipher_migrate;";
+			var migrateResult = commandMigrate.ExecuteNonQuery();
+			
+			var commandInsert = sqLiteDbConnection.CreateCommand();
+			commandInsert.CommandText = "INSERT INTO testTable(id) VALUES (1);";
+			commandInsert.ExecuteNonQuery();
+			
+			var commandQuery = sqLiteDbConnection.CreateCommand();
+			commandQuery.CommandText = "SELECT * FROM testTable;";
+			var queryResult = commandQuery.ExecuteScalar();
+			
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart() {
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep() {
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume() {
+			// Handle when your app resumes
+		}
+	}
+}
